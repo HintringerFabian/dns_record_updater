0c0eab206475942e59b717a487062ab42c029a72
Fabian Hintringer <fabianhintringer01@gmail.com>
2023-12-04 13:28:38 +0100

    implement iclude mode

--- EnvReader.py
+++ EnvReader.py
@@ -1,3 +1,5 @@
+import re
+
 from dotenv import dotenv_values
 
 
@@ -5,13 +7,20 @@ class EnvReader:
     INCLUDE = "INCLUDE"
     EXCLUDE = "EXCLUDE"
 
+    @staticmethod
+    def __prepare_record(input_string: str) -> str:
+        # Use regular expression to replace whitespaces, newlines, and tabs with an empty string
+        pattern = re.compile(r'\s+')
+        result = re.sub(pattern, '', input_string)
+        return result
+
     def __init__(self):
         self.env = dotenv_values(".env")
         self.domain = self.env["DOMAIN"]
         self.api_secret = self.env["API_SECRET"]
         self.shopper_id = self.env["SHOPPER_ID"]
         self.api_key = self.env["API_KEY"]
-        self.dns_records = self.env["DNS_RECORDS"].split(",")
+        self.dns_records = [self.__prepare_record(record) for record in self.env["DNS_RECORDS"].split(",")]
 
         self.mode = self.env.get("MODE", self.INCLUDE)
 
--- RecordUpdater.py
+++ RecordUpdater.py
@@ -12,6 +12,11 @@ def remove_excluded_records(records, env):
     return [record["name"] for record in records if record["name"] not in excluded_records]
 
 
+def include_records(dns_records, env):
+    included_records = env.dns_records
+    return [record["name"] for record in dns_records if record["name"] in included_records]
+
+
 def update(record, new_ip, env):
     url = f"https://api.godaddy.com/v1/domains/{env.domain}/records/A/{record}"
 
@@ -39,13 +44,18 @@ def update(record, new_ip, env):
         logger.print_and_log(f"DNS record {record} now has the ip address {new_ip}")
 
 
+def filter_records(dns_records, env):
+    mode = env.mode
+
+    if mode == EnvReader.INCLUDE:
+        return include_records(dns_records, env)
+    elif mode == EnvReader.EXCLUDE:
+        return remove_excluded_records(dns_records, env)
+
+
 def update_records(new_ip):
     env = EnvReader()
-
-    dns_records = get_dns_records()
-
-    if env.mode == env.EXCLUDE:
-        dns_records = remove_excluded_records(dns_records, env)
+    dns_records = filter_records(get_dns_records(), env)
 
     for record in dns_records:
         update(record, new_ip, env)
